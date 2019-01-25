ff87ca3e8479bd78a892d8ec431e42c4c07808f0
thaycacac <thaycacac@gmail.com>
2019-01-25 22:57:46 +0700

    [fix] screen language

--- screens/LanguagesScreen.js
+++ screens/LanguagesScreen.js
@@ -1,48 +1,82 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import { List, ListItem, Text, Icon } from 'react-native-elements'
 
 const list = [
   {
     title: 'English',
-    value: 'Eng',
+    value: 'en',
   },
   {
     title: 'Tiếng Việt',
-    value: 'VN',
+    value: 'vi',
   },
   {
     title: 'Laos',
-    value: 'Lao',
+    value: 'la',
   },
   {
     title: 'Japanese',
-    value: 'Jav',
+    value: 'jp',
   },
 ]
 
 export default class Languages extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      // eslint-disable-next-line react/no-unused-state
+      langCurrent: 'en',
+    }
+  }
+
+  changLanguage = language => {
+    this.setState({
+      langCurrent: language,
+    })
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, backgroundColor: '#fff' }}>
-        <View style={{ margin: 20 }}>
-          <Text h2>Language</Text>
-          <List>
-            {list.map(item => {
-              if (item.value === 'Eng') {
-                return (
-                  <ListItem
-                    title={item.title}
-                    titleStyle={{ color: '#FD5739', fontStyle: 'bold' }}
-                    rightIcon={<Icon name="check" color="#FD5739" />}
-                  />
-                )
-              }
-              return <ListItem title={item.title} hideChevron />
-            })}
-          </List>
-        </View>
+      <View style={styles.wrapContent}>
+        <Text h2>Language</Text>
+        <List>
+          {list.map(item => {
+            if (this.state.langCurrent === item.value) {
+              return (
+                <ListItem
+                  key={item.value}
+                  title={item.title}
+                  titleStyle={styles.langCurrent}
+                  onPress={() => this.changLanguage(item.value)}
+                  rightIcon={<Icon name="check" color="#FD5739" />}
+                />
+              )
+            } else
+              return (
+                <ListItem
+                  key={item.value}
+                  title={item.title}
+                  hideChevron
+                  onPress={() => this.changLanguage(item.value)}
+                />
+              )
+          })}
+        </List>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  wrapContent: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 20,
+  },
+  langCurrent: {
+    color: '#FD5739',
+    fontWeight: 'bold',
+  },
+})
